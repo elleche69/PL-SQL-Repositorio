ed334c780517f9ecacf09d28b5cff6d93a74cf0a
elleche69 <ispas.decebalaitor@koldomitxelena.net>
2016-03-03 12:16:23 +0100

    DD

--- p1q5.sql
+++ p1q5.sql
@@ -1,30 +1,64 @@
 --5--
-  select * from centros;
+
+/*  select * from centros;
 
 
 
   DECLARE
     vnombre CENTROS.NOMBRE%TYPE := 'ABRERA';
     vprovincia CENTROS.PROVINCIA%TYPE := 'ARABA';
-    v_nombre CENTROS.NOMBRE%TYPE;
     v_id CENTROS.ID%TYPE;
   
   BEGIN
-  
-    SELECT ID,NOMBRE INTO v_id, v_nombre FROM CENTROS
-    WHERE NOMBRE = vnombre;
-    
+      
     IF(SQL%FOUND) THEN
       UPDATE CENTROS SET PROVINCIA = vprovincia 
-      where ID = v_id;
-      commit;      
+      where UPPER(Nombre) = vnombre;
+      commit;  
+      
+     
      END IF;    
       
      
-   END;
+   END;*/
   
+--6--
+
+
+   --Esta select no debe de ir en el BEGIN ya que esta devuelve mas de una fila--
+
   
+  CREATE OR REPLACE PROCEDURE VER_trabajador IS
+      
+      
+      CURSOR emp_cursor is 
+          Select APELLIDO1, APELLIDO2, FECHANAC    
+          From TRABAJADORES order by Apellido1;
+          
+      emp_record emp_cursor%ROWTYPE;
+      
+      
+
+   BEGIN  
+        Open emp_cursor;        
+        LOOP 
+        
+          FETCH emp_cursor into emp_record;
+            DBMS_OUTPUT.PUT_LINE('Apellidos del trabajador: ' || emp_record.apellido1 || emp_record.apellido2 || to_char(emp_record.fechanac));            
+          EXIT WHEN emp_cursor%NOTFOUND;
+          
+        END LOOP;
+      
+      
+    
   
+  END;
   
+
+ execute Ver_trabajador;
  
-  
+ Select * from TRABAJADORES;
+ 
+ 
+ 
+
